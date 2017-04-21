1b0e910423f6af60e813f6469eeac67100be0cb8
faviolannyrath <faviolannyrath@gmail.com>
2017-04-20 18:52:33 -0700

    import

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+  
+$(document).ready(function() {
+  
+  $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 142) {
+      $('.navbar-container').addClass('navbar-fixed');
+      $('#blogcontainer').css("padding-top", "47px")
+    }
+    if ($(window).scrollTop() < 143) {
+      $('.navbar-container').removeClass('navbar-fixed');
+      $('#blogcontainer').css("padding-top", "0px")
+    }
+  });
+});
