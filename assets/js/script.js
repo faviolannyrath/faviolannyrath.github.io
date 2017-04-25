8ede1061ef5567c7c9347b00a2369760c29a2732
faviolannyrath <faviolannyrath@gmail.com>
2017-04-24 18:51:34 -0700

    blog content

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,7 +48,7 @@ function topFunction() {
       console.log($(window).scrollTop())
     if ($(window).scrollTop() > 142) {
       $('.navbar-container').addClass('navbar-fixed');
-      $('#blogcontainer').css("padding-top", "47px")
+      $('#blogcontainer').css("padding-top", "81px")
     }
     if ($(window).scrollTop() < 143) {
       $('.navbar-container').removeClass('navbar-fixed');
